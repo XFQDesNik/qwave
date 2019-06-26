675138a877f523768fd27c12b5b6f7e46d3ce336
Artem Afonin <xfqdesnik@mail.ru>
2019-06-27 00:25:16 +0300

    Add Security

---
+++ src/main/java/com/example/qwave/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.qwave.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/com/example/qwave/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.qwave.repos;
+
+import com.example.qwave.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
